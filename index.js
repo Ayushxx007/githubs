4ed9988ab952f4e35a1d9ed74cce93a90638f540
“Ayush <ayushkumawat77889@gmail.com>
2025-03-21 07:33:26 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,4 @@
+function commit1(){
+  console.log('commit1');
+};
+commit1();
---
+++ index.py
@@ -0,0 +1,9 @@
+print("hello world");
+
+for i in range(10):
+   
+    if i == 5:
+        continue;
+    
+    print(i);
+ 
